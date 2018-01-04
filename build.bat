147676c0a4608efb3032f624a353249eab008b4d
henkman <henkman@github.com>
2018-01-04 20:47:50 +0100

    build from zero

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
-@go build -ldflags="-s -w -H windowsgui" serverbrowser.go && go build -ldflags="-s -w -H windowsgui" serverstats.go && go build -ldflags="-s -w" autojoiner.go
+@go get -u -v
+@go build -ldflags="-s -w -H windowsgui" serverbrowser.go
+@go build -ldflags="-s -w -H windowsgui" serverstats.go
+@go build -ldflags="-s -w" autojoiner.go
